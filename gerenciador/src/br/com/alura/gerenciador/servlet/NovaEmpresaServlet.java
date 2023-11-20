884add9ac3b8d518d72636ac2c343463bc711067
Matheus Koyama <koyamaCRS@gmail.com>
2023-11-20 15:50:35 -0300

    Aplicação com JSP

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,8 +31,10 @@ public class NovaEmpresaServlet extends HttpServlet {
       Banco banco = new Banco();
       banco.adiciona(empresa);   
        
-      PrintWriter out = response.getWriter();
-      out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+     //chamar o JPS
+     RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+     request.setAttribute("empresa", empresa.getNome());
+     rd.forward(request, response);
 	}
 
 }
