fc49e1f6a27232dc9f9e6bb80a72016916bef987
Matheus Koyama <koyamaCRS@gmail.com>
2023-11-24 20:14:28 -0300

    Aplicando Filtros

--- gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -23,7 +23,7 @@ import br.com.alura.gerencidor.acao.NovaEmpresaForm;
 import br.com.alura.gerencidor.acao.RemoveEmpresa;
 
 
-@WebServlet("/entrada")
+//@WebServlet(urlPatterns="/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -34,38 +34,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 		
 		String paramAcao = request.getParameter("acao");
          
-		HttpSession sessao = request.getSession();
-		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
-		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.contentEquals("LoginForm"));
+//		HttpSession sessao = request.getSession();
+//		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+//		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.contentEquals("LoginForm"));
+//		
+//		if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
+//	
+//			response.sendRedirect("entrada?acao=LoginForm");
+//			return;
+//		}
 		
-		if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
-	
-			response.sendRedirect("entrada?acao=LoginForm");
-			return;
-		}
 		
 		
-		String nomeDaClasse = "br.com.alura.gerencidor.acao."+paramAcao;
-				
-		String nome;
-		try {
-			Class classe = Class.forName(nomeDaClasse);//Carrega a classe com o nome
-			Acao acao = (Acao) classe.newInstance();	
-			nome = acao.executa(request,response);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-			throw new ServletException(e);
-		}
-	    	
-		
-		 String[] tipoEEndereco =  nome.split(":");
-		 if(tipoEEndereco[0].equals("forward")) {
-		  
-			 RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" +tipoEEndereco[1]);
-		     rd.forward(request, response);
-		 }else {
-		        response.sendRedirect(tipoEEndereco[1]);
-
-		 }
 		 
 		//paramAcao.executa(req,res);
 	
